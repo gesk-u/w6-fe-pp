45b2cd1405d0c47abc7e8750b63765098ea1935c
gesk-u <anastasiia@example.com>
2026-09-21 14:38:37 +0300

    [iter1] Complete setup ...

---
+++ frontend/src/components/BookListings.jsx
@@ -0,0 +1,12 @@
+import BookListing from "./BookListing";
+
+const BookListings = () => {
+  return (
+    <div className="book-list">
+      <BookListing />
+    </div>
+  );
+};
+
+export default BookListings;
+
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import BookListings from "../components/BookListings";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <BookListings />
+    </div>
+  );
+};
+
+export default Home;
+
