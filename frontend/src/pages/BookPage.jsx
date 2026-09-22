4f2854f7c5da3b21841e6a16c832be1707bc6f82
gesk-u <anastasiia@example.com>
2026-09-22 13:45:21 +0300

    feat(book-page): add route and fetch single book by id

--- frontend/src/pages/BookPage.jsx
+++ frontend/src/pages/BookPage.jsx
@@ -1,16 +1,47 @@
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
 const BookPage = () => {
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+  const fetchBook = async () => {
+    try {
+      const res = await fetch(`/api/books/${id}`);
+      if (!res.ok) throw new Error("Network response was not ok");
+      const data = await res.json();
+      setBook(data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchBook();
+  }, [id])
+
   return (
-    <div className="book-preview">
-      <>
-        <h2></h2>
-        <p>Author: </p>
-        <p>ISBN: </p>
-        <p>Publisher: </p>
-        <p>Genre: </p>
-        <p>Available: </p>
-        <p>Due Date: </p>
-        <p>Borrower: </p>
-      </>
+    <div>
+      {book && (
+        <div className="book-preview">
+          <h2>{book.title}</h2>
+          <p>Author: {book.author}</p>
+          <p>ISBN: {book.isbn}</p>
+          <p>Publisher: {book.publisher}</p>
+          <p>Genre: {book.genre}</p>
+          <p>Available: {book.availability.isAvailable ? "Yes" : "No"}</p>
+          <p>Due Date: {book.availability.dueDate
+            ? new Date(book.availability.dueDate).toLocaleDateString()
+            : "—"}
+          </p>
+          <p>Borrower: {book.availability.borrower || "—"}</p>
+          <button onClick={() => navigate("/")}>Back</button>
+        </div>
+      )}
     </div>
   );
 };
