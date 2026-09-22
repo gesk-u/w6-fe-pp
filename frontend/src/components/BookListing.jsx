49ce1e64ab87d4aa268fe65d34916922b92f4a6a
gesk-u <anastasiia@example.com>
2026-09-22 13:49:31 +0300

    feat(book-page): add route and fetch single book by id

--- frontend/src/components/BookListing.jsx
+++ frontend/src/components/BookListing.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const BookListing = ({ book }) => {
   return (
     <div className="book-preview">
-      <h2>{book.title}</h2>
+      <Link to={`/books/${book.id}`}>
+        <h2>{book.title}</h2>
+      </Link>
       <p>Author: {book.author}</p>
       <p>ISBN: {book.isbn}</p>
       <p>Publisher: {book.publisher}</p>
