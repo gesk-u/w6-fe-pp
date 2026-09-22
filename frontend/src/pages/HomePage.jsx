024b73a9737a2b2b5bcfb366cc755ed4b18f707b
Stefan <Apollo71@abv.bg>
2026-09-22 13:33:22 +0300

    feat(list-books): fetch and display all books on HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,35 @@
 import BookListings from "../components/BookListings";
+import { useState, useEffect } from "react";
+
 
 const Home = () => {
+
+  const [books, setBooks] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch("/api/books");
+        if (!response.ok) throw new Error("Could not fetch books");
+        const data = await response.json();
+        setBooks(data);
+        setIsPending(false);
+      } catch (err) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    };
+    fetchBooks();
+  }, []);
+
   return (
     <div className="home">
-      <BookListings />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {books && <BookListings books={books} />}
     </div>
   );
 };
